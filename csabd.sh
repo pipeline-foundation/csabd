cb04cb29e3bd8222bbc9c6ec776d5f3ee4e6ea5c
AleksIvanovSinglet <Aleksandar.Ivanov@singlet.dev>
2021-02-09 12:38:17 +0200

    add proper regex and echo

--- csabd.sh
+++ csabd.sh
@@ -15,26 +15,25 @@ echo $ARRAY_OF_ALL_ALERTS
 
 for index in ${ARRAY_OF_ALL_ALERTS[@]}
 do
-	echo "'"$index"'"
+	echo $index
 
-	REGEX_PAT='\d+'
-	[[ "'"$index"'" =~ $REGEX_PAT ]]
+	echo $index | sed 's/[^0-9]*//g'
 
-	echo This is BASH_REMATCH[0]: "${BASH_REMATCH[0]}"
+	INDEX_CLEAN = $index | sed 's/[^0-9]*//g'
 
-	echo This is BASH_REMATCH[1]: "${BASH_REMATCH[1]}"
+	echo $INDEX_CLEAN
 
-	ALERT_PATH=$(curl -u "$OWNER":"$ACCESS_TOKEN" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$OWNER/$PROJECT_NAME/code-scanning/alerts/${BASH_REMATCH[1]}" | jq .instances[0].location.path)
+	ALERT_PATH=$(curl -u "$OWNER":"$ACCESS_TOKEN" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$OWNER/$PROJECT_NAME/code-scanning/alerts/$INDEX_CLEAN" | jq .instances[0].location.path)
 
 	if [[ "$ALERT_PATH" == *"$ALERT_DIS_PATH"* ]]; then
 
-		ALERT_URL="https://api.github.com/repos/$OWNER/$PROJECT_NAME/code-scanning/alerts/${BASH_REMATCH[1]}"
+		ALERT_URL="https://api.github.com/repos/$OWNER/$PROJECT_NAME/code-scanning/alerts/$INDEX_CLEAN"
 
 		curl -u "$OWNER":"$ACCESS_TOKEN" -X PATCH -H "Accept: application/vnd.github.v3+json" $ALERT_URL -d '{"state":"dismissed","dismissed_reason":"'"$DISMISS_REASON"'"}'
 
-		echo Alert \#${BASH_REMATCH[1]} is at "$ALERT_DIS_PATH"
+		echo Alert \#$INDEX_CLEAN is at "$ALERT_DIS_PATH"
 
 	else
-		echo Alert \#${BASH_REMATCH[1]} is not at "$ALERT_DIS_PATH"
+		echo Alert \#$INDEX_CLEAN is not at "$ALERT_DIS_PATH"
 	fi
 done
