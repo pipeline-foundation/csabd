84a044f09ea6abf7b3f5ae41048be259e325f967
AleksIvanovSinglet <Aleksandar.Ivanov@singlet.dev>
2021-02-09 15:15:21 +0200

    remove unnecessary echos

--- csabd.sh
+++ csabd.sh
@@ -13,13 +13,7 @@ done
 
 for index in ${ARRAY_OF_ALL_ALERTS[@]}
 do
-	echo $index
-
-	echo $index | sed 's/[^0-9]*//g'
-
 	INDEX_CLEAN=`echo $index | sed 's/[^0-9]*//g'`
-
-	echo $INDEX_CLEAN
 
 	ALERT_PATH=$(curl -u "$OWNER":"$ACCESS_TOKEN" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$OWNER/$PROJECT_NAME/code-scanning/alerts/$INDEX_CLEAN" | jq .instances[0].location.path)
 
